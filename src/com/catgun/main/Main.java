6a9e74ac2ccd609209869a49f5ca6f8e4c18d636
Lee Delarm <protoshell@gmail.com>
2011-05-12 22:06:35 -0500

    added libs

--- src/com/catgun/main/Main.java
+++ src/com/catgun/main/Main.java
@@ -1,13 +1,16 @@
 package com.catgun.main;
 
+import com.badlogic.gdx.backends.android.AndroidApplication;
+import com.protoshell.main.CatGunGame;
+
 import android.app.Activity;
 import android.os.Bundle;
 
-public class Main extends Activity {
+public class Main extends AndroidApplication {
     /** Called when the activity is first created. */
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.main);
+        initialize(new CatGunGame(), false);
     }
 }
